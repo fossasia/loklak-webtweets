90d34c3e8d0f42a6d05031f7c5ca11c1bfa0a1c7
DengYiping <scottdeng@live.cn>
2016-01-07 10:43:50 +0800

    URI encoding support.
    For GCI task.

--- js/ngscript.js
+++ js/ngscript.js
@@ -2,7 +2,9 @@ var ngApp = angular.module('loklak', []);
 
 ngApp.controller("tweets", function($scope, $http) {
   $scope.results = [];
-  $http.get('http://loklak.org/api/search.json?q=%23fossasia').success(function(data, status, headers, config) {
+  var query = "#fossasia"; //modify this line to change the key word.
+    
+  $http.get('http://loklak.org/api/search.json?q=' + encodeURIComponent(query)).success(function(data, status, headers, config) {
       var i = 0;
     for (i = 0; i < data.statuses.length; i++) {
             $scope.results.push(data.statuses[i]);
