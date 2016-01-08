c3d71ecd5d1eb32eefe35dcd6fbbc50228941ebf
Anshuman Agarwal <Anshuman Agarwal>
2016-01-08 15:31:30 +0530

    Links are now rendered so and even loklak links are converted to normal ones

--- js/tweets.js
+++ js/tweets.js
@@ -21,5 +21,25 @@ function lastTweet() {
 }
 
 function parser(data) {
-	document.getElementById("tweets").innerHTML = "<p class='tweet'>" + data.statuses[tweetNum].text + "</p>";
+	var parsed = ""
+	var tweet = data.statuses[tweetNum].text;
+	var words = tweet.split(" ");
+	var loklakLinkCount = 0;
+	for (word in words) {
+		if (words[word].startsWith("@")) {
+			parsed += "<a href='https://twitter.com/" + words[word].slice(1) + "' target='_blank'>" + words[word] + "</a> ";
+		} else if (words[word].startsWith("#")) {
+			parsed += "<a href='https://twitter.com/hashtag/" + words[word].slice(1) + "' target='_blank'>" + words[word] + "</a> ";
+		} else if (words[word].startsWith("http")) {
+			if (words[word].startsWith("http://loklak")) {
+				parsed += "<a href='" + data.statuses[tweetNum].links[loklakLinkCount] + "' target='_blank'>" + data.statuses[tweetNum].links[loklakLinkCount] + "</a> ";
+				loklakLinkCount += 1;
+			} else {
+				parsed += "<a href='" + words[word] + "' target='_blank'>" + words[word] + "</a> ";
+			}
+		} else {
+			parsed += words[word] + " ";
+		}
+	}
+	document.getElementById("tweets").innerHTML = "<p class='tweet'>" + parsed + "</p>";
 }
